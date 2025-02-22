ae3c2cf343272cc1be67eccedc08907b1ec0e46a
SRC-Arts <gabiitheweirdo@gmail.com>
2025-02-22 02:49:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function showInfo() {
-    document.querySelector('.container').style.display = 'none';
-    document.querySelector('.info-page').style.display = 'block';
-    document.querySelector('.services-page').style.display = 'none';
-    document.querySelector('.contact-page').style.display = 'none';
-}
-
 function showHome() {
     document.querySelector('.info-page').style.display = 'none';
     document.querySelector('.services-page').style.display = 'none';
     document.querySelector('.contact-page').style.display = 'none';
     document.querySelector('.container').style.display = 'block';
+}
+
+function showInfo() {
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.info-page').style.display = 'block';
+    document.querySelector('.services-page').style.display = 'none';
+    document.querySelector('.contact-page').style.display = 'none';
 }
 
 function showService() {
